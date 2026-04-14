16e521cf8bdb8bc7d0780a571abe2391bc2e5c46
Aaron Payne <aaron.payne@my.uwi.edu>
2026-04-14 14:25:49 -0400

    remove img url from db

--- app/models/workout.py
+++ app/models/workout.py
@@ -10,9 +10,11 @@ class WorkoutBase(SQLModel):
     muscle_group: str   # e.g. "chest", "legs", "back"
     difficulty: str     # e.g. "beginner", "intermediate", "advanced"
     equipment: str      # e.g. "none", "dumbbells", "barbell"
+    # demo_img_url: Optional[str] = None
 
 class Workout(WorkoutBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     created_by: Optional[int] = Field(default=None, foreign_key="user.id")
+
 
     routine_workouts: List["RoutineWorkout"] = Relationship(back_populates="workout")
